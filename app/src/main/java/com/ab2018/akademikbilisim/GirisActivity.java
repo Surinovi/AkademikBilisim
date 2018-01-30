ae1239bb68cc14f8bea30bb50459cb5f5182ec46
Musatafaa <m.bingul@gtu.edu.tr>
2018-01-30 15:37:58 +0300

    giris ve kayit güncelleme

--- app/src/main/java/com/ab2018/akademikbilisim/GirisActivity.java
+++ app/src/main/java/com/ab2018/akademikbilisim/GirisActivity.java
@@ -1,13 +1,30 @@
 package com.ab2018.akademikbilisim;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GirisActivity extends AppCompatActivity {
-
+  Button giris;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_giris);
+
+
+        giris=findViewById(R.id.btn_signup);
+        giris.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),ProfilOlusturActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }
